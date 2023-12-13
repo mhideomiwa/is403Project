4d3d3b1a6a30bd15b81b50651ddcaef1b2034826
mhmiwamiwaus <mhmiwa@miwa.us>
2023-12-12 18:28:53 -0700

    Maybe fixed the playloist?

--- public/pages/playlist.js
+++ public/pages/playlist.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    // alert("This is the playlist page 1");
     try {
-        const response = await fetch('/letsDate', {
+        const response = await fetch(getEndpoint(), {
             method: 'POST',
             body: '',
             headers: {
@@ -18,7 +17,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     } catch (error) {
         console.error('Fetch error:', error);
     }
-
-    // Test alert to make sure this is loading
-    // alert("This is the playlist page");
 });
+
+function getEndpoint() {
+    // Logic to determine the endpoint based on current URL or any other criterion
+    // For example:
+    const currentPath = window.location.pathname;
+
+    // Customize this logic based on how your routes are structured
+    if (currentPath === '/dance') {
+        return '/dance'; // Endpoint for the dance playlist
+    } else if (currentPath === '/gym') {
+        return '/gym'; // Endpoint for the gym playlist
+    } else if (currentPath === '/study') {
+        return '/study'; // Endpoint for the study playlist
+    } else if (currentPath === '/simp') {
+        return '/simp'; // Endpoint for the simp playlist
+    }
+    else {
+        return '/letsDate'; // Default endpoint
+    }
+}
